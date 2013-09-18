87185d025fa6594f30343e863a767da8777c255d
Alexi Nedeltchev <alexi_dst@yahoo.com>
2013-09-18 15:32:26 -0700

    unicorn, foreman, postgress

---
+++ README.md
@@ -0,0 +1,19 @@
+# Boilerplate Readme 
+
+This is un extremely basic Rails 4.0 application and Ruby 2.0, which includes:
+
+Unicorn - web server
+
+Postgress - database
+
+Foreman - process management tool
+
+##Run the application
+
+Start the unicorn server
+
+	rake db:create
+    foreman start
+
+
+Enjoy!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boilerplate::Application.config.secret_key_base = '90c81e4b6fc250562573d4abb7ac84f9fc1e75d9562dba5c9d4b3d6a898212a4798860f543fea67e5b21b9a448a1fe1a8db349384314be836dc1278a09887b17'
